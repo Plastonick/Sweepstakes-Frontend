79c832be492504756f1f37fcc974bac40a6792ac
David Pugh <plastonick@gmail.com>
2022-11-13 17:11:20 +0000

    collapsible sections

--- src/App.svelte
+++ src/App.svelte
@@ -125,6 +125,15 @@
   }
 
   const emojiTitle = 'Emoji should be a comma separated list of emoji randomly picked for their event, e.g. tada,confetti_ball,partying_face,fireworks'
+  let teamsExpanded = true
+  const toggleTeams = function () {
+    teamsExpanded = !teamsExpanded
+  }
+
+  let helpExpanded = true
+  const toggleHelp = function () {
+    helpExpanded = !helpExpanded
+  }
 </script>
 
 <Github/>
@@ -134,27 +143,41 @@
     World Cup 2022 Sweepstakes Announcer
   </h1>
 
-  <h5>
-    Add an announcer for match events to your Slack or Discord World Cup sweepstakes channel
-  </h5>
 
-  <p>
-    This bot posts to your Slack or Discord channel for any match event during the World Cup.
-    <br>
-    Events include: <i>match starting</i>, <i>goal scored</i> (and goal disallowed!), and <i>match finishing</i> events (declaring the winner, or a draw)
-  </p>
+  <hr>
+  <div
+      class="section-expand"
+      on:click={toggleHelp}
+  >
+    <span>Usage</span>
+    <span class="flex-space"></span>
+    <span class="status {helpExpanded ? 'expanded' : ''}">＋</span>
+  </div>
+  <hr>
 
-  <p>
-    To use the bot, create an incoming webhook for your Slack or Discord channel and enter it below.
-    <br>
-    If you already have a configuration, enter your webhook URL and click on the ⬇ button next to it to load your configuration.
-    <br>
-    Then, enter or update the emoji list if desired, and the names of the players for each team.
-    <br>
-    Emoji list should be a comma separated list of emojis which will be randomly selected for the related event.
-    <br>
-    Names should either be the player's handle such as <i>@dpugh</i> or the player name <i>David Pugh</i>
-  </p>
+  <div class="form-help collapsible-wrapper {helpExpanded ? '' : 'collapsed'}">
+    <h5>
+      Add an announcer for match events to your Slack or Discord World Cup sweepstakes channel
+    </h5>
+
+    <p>
+      This bot posts to your Slack or Discord channel for any match event during the World Cup.
+      <br>
+      Events include: <i>match starting</i>, <i>goal scored</i> (and goal disallowed!), and <i>match finishing</i> events (declaring the winner, or a draw)
+    </p>
+
+    <p>
+      To use the bot, create an incoming webhook for your Slack or Discord channel and enter it below.
+      <br>
+      If you already have a configuration, enter your webhook URL and click on the ⬇ button next to it to load your configuration.
+      <br>
+      Then, enter or update the emoji list if desired, and the names of the players for each team.
+      <br>
+      Emoji list should be a comma separated list of emojis which will be randomly selected for the related event.
+      <br>
+      Names should either be the player's handle such as <i>@dpugh</i> or the player name <i>David Pugh</i>
+    </p>
+  </div>
 
   <form on:submit|preventDefault>
     <div class="form-section">
@@ -234,9 +257,18 @@
     </div>
 
     <hr>
-
-    <div class="form-section">
-      {#each Object.entries(teams) as [ tla, name ]}
+    <div
+        class="section-expand"
+        on:click={toggleTeams}
+    >
+      <span>Team Selection</span>
+      <span class="flex-space"></span>
+      <span class="status {teamsExpanded ? 'expanded' : ''}">＋</span>
+    </div>
+    <hr>
+    <div class="collapsible-wrapper {teamsExpanded ? '' : 'collapsed'}">
+      <div class="form-section">
+        {#each Object.entries(teams) as [ tla, name ]}
         <span class="form-element">
         <label for="team_{tla}">{name}</label>
         <input
@@ -245,7 +277,8 @@
             bind:value={formValues.owners[tla]}
         />
         </span>
-      {/each}
+        {/each}
+      </div>
     </div>
 
     <div class="submit-section">
